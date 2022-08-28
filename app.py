6cf756810ea7b7f2fe23b9ba508aeb9e93dc16a1
joe gillard <joegillard3@gmail.com>
2022-08-28 17:04:20 +0100

    basic clock functionality done and tkinter window size and creation location done and disabled maximise button

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from cProfile import label
+from logging import PlaceHolder
 import tkinter as tk
+from tkinter import *
 import time
-
-
-
-epoch_time = time.time()
-local_time = time.ctime(epoch_time)
-
-print("The local time is:", local_time)
-
 
 root = tk.Tk()
 root.title("Digital clock")
 root.eval("tk::PlaceWindow . center")
+root.resizable(0,0)
+root.geometry("500x200")
 
-frame1 = tk.Frame(root, width=600, height=300)
-frame1.grid(row=0, column=0)
+lblClock = tk.Label(root,text = "",font=("raleway", 40))
+lblClock.place(relx=0.5,rely=0.5,anchor=CENTER)
 
+def clock():
+   hh= time.strftime("%I")
+   mm= time.strftime("%M")
+   ss= time.strftime("%S")
+   day=time.strftime("%A")
+   ap=time.strftime("%p")
+   lblClock.config(text= hh + ":" + mm +":" + ss + " " + ap)
+   lblClock.after(500,clock)
 
+clock()    
 
 root.mainloop()
